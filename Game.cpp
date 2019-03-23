dec2501fe4e2d5639fc54adb05f9991c62717317
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2019-03-23 20:30:09 +0200

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -2,13 +2,12 @@
 /* Noy Tvili 308426790 */
 using namespace std;
 #include "Game.h"
-//#include "Player.h"
+#include "Player.h"
 
 
  void Game::start(){
     int i,num,num_players;
     string player_name;
-    vector<Player>::iterator turn = players_list.begin();
 
     cout << "How many players?" << endl;
     cin >> num_players;
@@ -19,9 +18,20 @@ using namespace std;
         cout << "player number " << i+1 << " name?" << endl;
         cin >> player_name;
         Player p(player_name, num);
-        players_list.push_back(p);
+        this->players_list.push_back(p);
+
 
     }
+    vector<Player>::iterator turn = players_list.begin();
     Card curr=generate_card();
     bool ans = turn->play(curr);
+}
+
+void Game::print_players(){
+    vector<Player>::iterator it = players_list.begin();
+    	    while(it != players_list.end()){
+                cout << *it << " ";
+                it++;
+	    }
+	   cout<<endl;
 }
