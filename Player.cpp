5a61f632430b18d1aa9a7853f7585c88580223a5
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2019-03-26 10:26:26 +0200

    Update Player.cpp
    new : isWinner , options.
    update : function play call options. 👍

--- Player.cpp
+++ Player.cpp
@@ -11,7 +11,7 @@
 		
 		for(j=0; j<n; j++){
 		    Card c(generate_card());
-            	cards.push_back(c);
+            cards.push_back(c);
         }
 	}
 	
@@ -28,16 +28,50 @@
 	   cout<<endl;
 	}
 	
+	void Player::options(Card &current){//options when the player pick a card
+	    int choice;
+	    bool flag =true;
+	    
+	    while(flag){
+	        cin>>choice;
+	        if(choice==0 || choice>cards.size()){ // not in range
+	            Card new_card(generate_card());
+	            cards.push_back(new_card);
+	            flag-false;//next turn
+	        }
+	        else{
+	            if(current.is_legal(cards.at(choice-1))){ // if he picks legal card
+	                current= (cards.at(choice-1));
+	                cards.erase(cards.begin()+(choice-1));
+	                flag=false; //next turn
+	            }
+	            else{ // not legal card
+	                cout << "you can't put " << cards.at(choice-1) << " on " << current << endl;;
+	            }
+	        }
+	        
+	    }
+	    
+	}
+	
     string Player::get_name(){
         return name;
     }
 
-    bool Player::play(Card &current){
+    bool Player::play(Card &current){ //player playing
         cout << "current: " << current << endl;
         cout << this->get_name() <<", your turn - " << endl;
         show_cards();
+        options(current);
         
     return true;
+    }
+    
+    bool Player::isWinner(){ //true if wins , false if not
+    if(cards.size()==0){
+        return true;
+    }
+    else return false;
     }
     
     bool Player::operator!= ( Player& p2){
