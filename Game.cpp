9af6e121f4929bdcdae591fbb2f7d9d66beece20
noytvili <33618257+noytvili@users.noreply.github.com>
2019-03-28 23:28:23 +0200

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -4,7 +4,6 @@ using namespace std;
 #include "Game.h"
 #include "Player.h"
 
-
  void Game::start(){
     int i,num,num_players;
     string player_name;
@@ -14,17 +13,46 @@ using namespace std;
     cout << "How many cards?" << endl;
     cin >>  num;
 
-    for(i=0; i<num_players; i++){
+    for(i = 0; i < num_players; i++){
         cout << "player number " << i+1 << " name?" << endl;
         cin >> player_name;
         Player p(player_name, num);
         this->players_list.push_back(p);
-
-
     }
+    
     vector<Player>::iterator turn = players_list.begin();
-    Card curr=generate_card();
-    bool ans = turn->play(curr);
+    Card curr = generate_card();
+
+    while(true){
+        bool ans = turn->play(curr);
+        if (curr.get_sign() == STOP){  //if the card is STP
+            if(turn->isWinner()) return; //this plater win!
+            if(players_list.size() == 2) turn = turn; //turn the same player
+            else if(turn == players_list.end()-1) turn = players_list.begin()+1; //השחקן האחרון עשה עצור על השחקן הראשון
+            else if(turn == players_list.end()-2) turn = players_list.begin(); //השחקן לפני האחרון עצר את האחרון
+            else
+                turn = turn+2; //כל שאר המקרים הלא מיוחדים
+        }
+        else if(curr.get_sign() == PLUS){
+            if(turn->isWinner()) return; //this plater win!
+            turn = turn; //the same player turn again
+        }
+        
+        else if(curr.get_sign() == CD){ //if the card is change direction
+            if(turn->isWinner()) return; //this plater win!
+            if(turn == players_list.begin()){
+                reverse(players_list.begin(), players_list.end());
+                turn = players_list.end(); //הראשון שם שנה כיוון ועכשיו תור האחרון
+                
+            }
+        }
+        else{ //other sign (not special sign)
+            if(turn->isWinner()) return; //this plater win!
+            if(turn == players_list.end()-1) turn = players_list.begin();
+            else turn++;
+        }
+        
+    }  //end while
 }
 
 void Game::print_players(){
